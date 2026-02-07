e2bba428438074cf2cddb8507f3e4cd4315c5ef6
Emir <45953377+emi-ced@users.noreply.github.com>
2026-02-07 22:31:49 +0100

    Update BellmanFord.cs

--- Algorithms/Graph Algorithms/BellmanFord.cs
+++ Algorithms/Graph Algorithms/BellmanFord.cs
@@ -12,8 +12,11 @@
     {
         Dictionary<int, int> nodeDistance = new();
         for (int i = 1; i <= n; i++)
+        {
+            // If we specified '-1' instead of 'int.MaxValue' we would never get the solution.
+            // 'nodeDistance[edge[0]] + edge[2] < nodeDistance[edge[1]]' would never be true becuase '-1' is always smaller.
             nodeDistance.Add(i, int.MaxValue);
-
+        }    
         nodeDistance[k] = 0;
 
         // Define min distance to each node.
