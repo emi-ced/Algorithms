6b637c667e24808c424278b0e81db157efea6b6b
Emir <45953377+emi-ced@users.noreply.github.com>
2026-06-16 10:32:10 +0200

    Update QuickSort.cs

--- Algorithms/Sorting Algorithms/QuickSort.cs
+++ Algorithms/Sorting Algorithms/QuickSort.cs
@@ -16,12 +16,20 @@
             if (startIndex >= endIndex)
                 return;
 
+            // Random pivot index.
+            int pivotIndex = Random.Shared.Next(startIndex, endIndex);
+
+            // Swap random pivot to the end of the array.
+            int tempPivot = nums[pivotIndex];
+            nums[pivotIndex] = nums[endIndex - 1];
+            nums[endIndex - 1] = tempPivot;
+            
             int pivotValue = nums[endIndex - 1];
             int nextAvailableSlot = startIndex;
 
             // We can't set 'i = startIndex + 1' because we have to define the value at 'startIndex' as smaller than pivot.
             // Otherwise the value will be moved as greater which is not always correct.
-            for (int i = startIndex; i < endIndex; i++)
+            for (int i = startIndex; i < endIndex - 1; i++)
             {
                 if (nums[i] < pivotValue)
                 {
@@ -44,17 +52,28 @@
     /// <summary>
     /// Quick-select is a variant of quick-sort where we sort only the part of the array where the target value is located.
     /// On every iteration in best case we remove half the elements of the array.
-    /// Time: Omega(n) - Theta(n) - O(n*n)
-    /// Space: Omega(logn) - Theta(logn) - O(n)
     /// </summary>
     public class QuickSelect
     {
-        private int QuickSelectInternal(int[] nums, int left, int right, int k)
+        // Time: Omega(n) - Theta(n) - O(n*n)
+        // Space: Omega(logn) - Theta(logn) - O(n)
+        private int QuickSelectRecursive(int[] nums, int left, int right, int k)
         {
+            if (left >= right)
+                return -1;
+            
+            // Random pivot index.
+            int pivotIndex = Random.Shared.Next(left, right);
+
+            // Swap random pivot to the end of the array.
+            int tempPivot = nums[pivotIndex];
+            nums[pivotIndex] = nums[right - 1];
+            nums[right - 1] = tempPivot;
+            
             var pivotValue = nums[right - 1];
             var swapIndex = left;
 
-            for (int i = left; i < right; i++)
+            for (int i = left; i < right - 1; i++)
             {
                 if (nums[i] < pivotValue)
                 {
@@ -70,11 +89,59 @@
             nums[swapIndex] = pivotValue;
 
             if ((nums.Length - k) < swapIndex)
-                return QuickSelectInternal(nums, left, swapIndex, k);
+                return QuickSelectRecursive(nums, left, swapIndex, k);
             else if ((nums.Length - k) > swapIndex)
-                return QuickSelectInternal(nums, swapIndex + 1, right, k);
+                return QuickSelectRecursive(nums, swapIndex + 1, right, k);
             else
                 return nums[swapIndex];
+        }
+
+        // Time: Omega(n) - Theta(n) - O(n*n)
+        // Space: Omega(1) - Theta(1) - O(1)
+        private int QuickSelectIterative(int[] nums, int left, int right, int k)
+        {
+            while (left < right)
+            {
+                // Random pivot index.
+                int pivotIndex = Random.Shared.Next(left, right);
+    
+                // Swap random pivot to the end of the array.
+                int tempPivot = nums[pivotIndex];
+                nums[pivotIndex] = nums[right - 1];
+                nums[right - 1] = tempPivot;
+
+                var pivotValue = nums[right - 1];
+                var swapIndex = left;
+
+                // Partition
+                for (int i = left; i < right - 1; i++)
+                {
+                    // Order descending.
+                    if (nums[i] > pivotValue)
+                    {
+                        int temp = nums[i];
+                        nums[i] = nums[swapIndex];
+                        nums[swapIndex] = temp;
+                        
+                        swapIndex++;
+                    }
+                }
+
+                nums[right - 1] = nums[swapIndex];
+                nums[swapIndex] = pivotValue;
+
+                // Target is in the left partition
+                if ((k -1) < swapIndex)
+                    right = swapIndex;
+                // Target is in the right partition
+                else if ((k -1) > swapIndex)
+                    left = swapIndex + 1;
+                // Found the target.
+                else
+                    return nums[swapIndex];
+            }
+
+            return -1;
         }
     }
 }
